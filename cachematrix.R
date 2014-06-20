f93c6991456dd3a6687cbe0569a5ba8e31c0197f
tkoksal <tkoksal@kent.edu>
2014-06-20 10:59:41 +0300

    ProgrammingAssignment2: Computing and Caching the inverse of a matrix, added few notes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix(), creates a list of 4 functions
+## that do the following:
+#   1. set the value of a matrix
+#   2. get the value of the matrix
+#   3. set the value of the inverse matrix
+#   4. get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() function computes the inverse of a matrix and when
+## the function is called for the second time, the inverse is 
+## retreived from the cache...
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("retrieving value from cache")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
+
+#### Testing cacheSolve()
+
+#Call the makeCacheMarix() function and assign it to 'm'
+m <- makeCacheMatrix()
+
+#use m's set function to create a matrix
+m$set(rbind(c(1, -1/4), c(-1/4, 1)))
+
+#use m's get function to retrieve the matrix created 
+m$get()
+
+#pass the list m to the cacheSolve() function: the inverse
+#  matrix should be returned
+cacheSolve(m)
+
+#pass the list m to the cacheSolve() function a second time
+# the inverse should be retreived from the cache accompanied by
+# a message "retrieving value from cache" 
+cacheSolve(m)
+
+### Reference:
+
+# The testing procedure imitated from Adam Gruer's post on 
+# the discussion forums
